0b685d90b01c52515cd3b55e76bba1eb71b752e6
stefanus ayudha <stefanus.ayudha@gmail.com>
2023-03-07 21:47:43 +0700

    add slider callback

--- app/src/main/java/com/example/testwarkopin/ui/modal/agentfilter/AgentFilterModal.kt
+++ app/src/main/java/com/example/testwarkopin/ui/modal/agentfilter/AgentFilterModal.kt
@@ -1,12 +1,16 @@
 package com.example.testwarkopin.ui.modal.agentfilter
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.accessibility.AccessibilityManager.TouchExplorationStateChangeListener
 import com.example.testwarkopin.databinding.LayoutAgentFilterBinding
 import com.example.testwarkopin.domain.payload.AgentPayload
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.slider.Slider
+import com.google.android.material.slider.Slider.OnSliderTouchListener
 
 class AgentFilterModal(
     private val preConfiguration: AgentPayload,
@@ -37,6 +41,21 @@ class AgentFilterModal(
 
         binding.slider.value =
             preConfiguration.minSoldItem.toFloat()
+
+        binding.slider.addOnSliderTouchListener(
+            object : OnSliderTouchListener {
+                @SuppressLint("RestrictedApi")
+                override fun onStartTrackingTouch(slider: Slider) {
+
+                }
+
+                @SuppressLint("RestrictedApi")
+                override fun onStopTrackingTouch(slider: Slider) {
+                    binding.minimumPoint.text = "Minimum Point : ${slider.value.toInt()}"
+                }
+
+            }
+        )
 
         binding.btnApply.setOnClickListener {
             onApply.invoke(
